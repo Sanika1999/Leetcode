7e7161f77933d32d98bc7bdb1bf9a2b2bc9c8518
Sanika <51399803+Sanika1999@users.noreply.github.com>
2020-10-05 00:46:41 +0530

    Added 1486

---
+++ 1486.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+class Solution {
+    public int xorOperation(int n, int start) {
+        int nums[]=new int[n];
+        for(int i=0;i<n;i++){
+            nums[i]=start+2*i;
+        }
+        int xor=0;
+        for(int j=0;j<n;j++){
+            xor=xor^nums[j];
+            
+        }
+        return xor;   
+    }
+}
